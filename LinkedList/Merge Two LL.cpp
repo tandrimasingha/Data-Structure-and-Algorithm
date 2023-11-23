5ca357081b32ea42640e60525c96aff217098bbd
Tandrima Singha <78292851+tandrimasingha@users.noreply.github.com>
2023-11-23 09:19:43 +0530

    Create Merge Two LL.cpp

---
+++ LinkedList/Merge Two LL.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+        if(list1->val>list2->val) swap(list1,list2);
+        ListNode* head=list1;
+        while(list1!=NULL && list2!=NULL)
+        {
+            ListNode* temp=list1;
+            while(list1!=NULL && list1->val<=list2->val)
+            {
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+        }
+      return head;
+    }
+};
